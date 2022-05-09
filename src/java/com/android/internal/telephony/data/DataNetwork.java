3315ac2e3bdcb3d243bb99dcb57c1c6947d164a6
Kiwon Park <kiwonp@google.com>
2022-05-09 21:59:45 +0000

    Collect unmetered networks information in PerSimStatus atom.
    Bug: 222704710
    Test: statsd_testdrive
    Change-Id: I6fb7fd7b3824e3a6705e4a723790a4feff99a7ee

--- src/java/com/android/internal/telephony/data/DataNetwork.java
+++ src/java/com/android/internal/telephony/data/DataNetwork.java
@@ -2729,6 +2729,10 @@ public class DataNetwork extends StateMachine {
         if (changed) {
             updateNetworkCapabilities();
         }
+        if (mTempNotMetered && isInternetSupported()) {
+            // NR NSA and NR have the same network type: NR
+            mDataCallSessionStats.onUnmeteredUpdate(networkType);
+        }
     }
 
     /**
