65cad86cc61a57e9c4f3eea4c61d52b31c151a75
Rambo Wang <rambowang@google.com>
2022-09-14 16:25:05 +0000

    Disable signal strength reporting when threshold is empty
    This change optimizes scenarios that when the signal threshold
    is empty, the SignalThresholdInfo is disabled before setting
    signal strength reporting criteria to modem.
    
    The issue can reproduce with fulfill of both of the conditions below:
    - Device is in idle mode
    - A customized SignalStrengthUpdateRequest was set with
    shouldReportWhileIdle to true and shouldReportSystemWhileIdle
    to false.
    
    In above scenario, the system signal thresholds which were not
    set by client may leave with empty threshold and enabled.
    The optimization here makes sure only the client's non-empty
    threshold is enabled.
    
    Bug: 245658506
    Test: atest  SignalStrengthControllerTest
    Test: feature regress test (go/signalstrength-s-ft)
    Merged-In: I22dceb4a3f34274942ca9a278d657c6d2263160c
    Change-Id: I22dceb4a3f34274942ca9a278d657c6d2263160c
    (cherry picked from commit b902b0ed0fb8c51f00d02077b72dabfcbd0decbd)

--- tests/telephonytests/src/com/android/internal/telephony/SimulatedCommands.java
+++ tests/telephonytests/src/com/android/internal/telephony/SimulatedCommands.java
@@ -2400,6 +2400,9 @@ public class SimulatedCommands extends BaseCommands
     @Override
     public void setSignalStrengthReportingCriteria(List<SignalThresholdInfo> signalThresholdInfos,
             Message result) {
+        SimulatedCommandsVerifier.getInstance().setSignalStrengthReportingCriteria(
+                signalThresholdInfos, result);
+        resultSuccess(result, null);
     }
 
     @Override
